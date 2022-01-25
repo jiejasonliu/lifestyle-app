eca79f9a79e3fc9ba287ec52e79aa85ea9152dcb
vijaymanbajracharya <bajracharyavijay@gmail.com>
2022-01-24 20:16:45 -0700

    Added a temporary login page that leads to home page

--- app/src/main/java/com/lifestyle/MainActivity.kt
+++ app/src/main/java/com/lifestyle/MainActivity.kt
@@ -2,10 +2,33 @@ package com.lifestyle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var loginButton : Button
+    private lateinit var username : EditText
+    private lateinit var password : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        username = findViewById(R.id.editTextTextUsername)
+        password = findViewById(R.id.editTextTextPassword)
+
+        loginButton = findViewById(R.id.buttonLogin)
+        loginButton.setOnClickListener{
+            if (username.text.isNullOrEmpty()||password.text.isNullOrEmpty()) {
+                Toast.makeText(this, "Please complete all the fields", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                intent.putExtra("username", username.text.toString())
+                startActivity(intent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/lifestyle/WelcomeActivity.kt
@@ -0,0 +1,25 @@
+package com.lifestyle
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.os.Handler
+
+class WelcomeActivity : AppCompatActivity() {
+    private lateinit var username : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        username = findViewById(R.id.textViewUsername)
+        username.text = intent.getStringExtra("username")
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }, 2000)
+    }
+}
