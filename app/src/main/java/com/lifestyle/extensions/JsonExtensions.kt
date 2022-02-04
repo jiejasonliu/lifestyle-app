f3af52537ab8544838230291a79b52fd4e1b6f69
jiejasonliu <jasonningliu@gmail.com>
2022-02-04 00:38:52 -0700

    Improve JSONArray.forEach extension function

--- app/src/main/java/com/lifestyle/extensions/JsonExtensions.kt
+++ app/src/main/java/com/lifestyle/extensions/JsonExtensions.kt
@@ -8,13 +8,14 @@ import org.json.JSONObject
  * USAGE:   import com.lifestyle.extensions.*
  */
 
+// enables --> for (jsonObj in jsonArr)
 @Throws(JSONException::class)
 operator fun JSONArray.iterator() {
     (0 until length()).asSequence().map { get(it) as JSONObject }.iterator()
 }
 
+// enables --> jsonArr.forEach { jsonObj -> ... }
 @Throws(JSONException::class)
 inline fun JSONArray.forEach(action: (JSONObject) -> Unit) {
-    (0 until length()).asSequence().map { get(it) as JSONObject }.iterator()
-        .forEach { action(it as JSONObject) }
+    (0 until length()).forEach { action(get(it) as JSONObject) }
 }
