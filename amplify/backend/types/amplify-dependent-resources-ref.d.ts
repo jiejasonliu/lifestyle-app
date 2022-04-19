70680f85e943ae0c3c29600a9bae0acc5c67ebb7
jiejasonliu <jasonningliu@gmail.com>
2022-04-18 20:36:26 -0600

    Initialize Amplify with S3 storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "lifestyle3425d4f6": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "CS4530RoomDbBackup": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
